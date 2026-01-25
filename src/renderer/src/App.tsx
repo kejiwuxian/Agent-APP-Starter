424ac779641f89ba5cefd8811322744e48f26814
kejiwuxian <1012072118@qq.com>
2026-01-25 12:42:15 -0800

    feat: setup example ui 'pnpx ai-elements@latest' 'pnpm add ai @ai-sdk/react zod'

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,279 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+'use client'
+import {
+  Conversation,
+  ConversationContent,
+  ConversationScrollButton
+} from '@/components/ai-elements/conversation'
+import {
+  Message,
+  MessageContent,
+  MessageResponse,
+  MessageActions,
+  MessageAction
+} from '@/components/ai-elements/message'
+import {
+  Attachment,
+  AttachmentPreview,
+  AttachmentRemove,
+  Attachments
+} from '@/components/ai-elements/attachments'
+import {
+  PromptInput,
+  PromptInputActionAddAttachments,
+  PromptInputActionMenu,
+  PromptInputActionMenuContent,
+  PromptInputActionMenuTrigger,
+  PromptInputBody,
+  PromptInputButton,
+  PromptInputHeader,
+  type PromptInputMessage,
+  PromptInputSelect,
+  PromptInputSelectContent,
+  PromptInputSelectItem,
+  PromptInputSelectTrigger,
+  PromptInputSelectValue,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputFooter,
+  PromptInputTools,
+  usePromptInputAttachments
+} from '@/components/ai-elements/prompt-input'
+import { Fragment, useState } from 'react'
+import { useChat } from '@ai-sdk/react'
+import { CopyIcon, GlobeIcon, RefreshCcwIcon } from 'lucide-react'
+import { Source, Sources, SourcesContent, SourcesTrigger } from '@/components/ai-elements/sources'
+import { Reasoning, ReasoningContent, ReasoningTrigger } from '@/components/ai-elements/reasoning'
+import { Loader } from '@/components/ai-elements/loader'
 
-function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+import { ThemeProvider } from '@/components/theme-provider'
+import { DirectChatTransport, ToolLoopAgent, ToolUIPart, createGateway, tool } from 'ai'
+import { z } from 'zod'
 
+import { Tool, ToolContent, ToolHeader, ToolInput, ToolOutput } from '@/components/ai-elements/tool'
+import { CodeBlock } from './components/ai-elements/code-block'
+
+const PromptInputAttachmentsDisplay = () => {
+  const attachments = usePromptInputAttachments()
+  if (attachments.files.length === 0) {
+    return null
+  }
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <Attachments variant="inline">
+      {attachments.files.map((attachment) => (
+        <Attachment
+          data={attachment}
+          key={attachment.id}
+          onRemove={() => attachments.remove(attachment.id)}
+        >
+          <AttachmentPreview />
+          <AttachmentRemove />
+        </Attachment>
+      ))}
+    </Attachments>
   )
 }
-
+const models = [
+  {
+    name: 'GPT 5 Nano',
+    value: 'openai/gpt-5-nano'
+  },
+  {
+    name: 'Mistral Nemo',
+    value: 'mistral/mistral-nemo'
+  },
+  {
+    name: 'Deepseek R1',
+    value: 'deepseek/deepseek-r1'
+  }
+]
+const App = () => {
+  const [input, setInput] = useState('')
+  const [model, setModel] = useState<string>(models[0].value)
+  const [webSearch, setWebSearch] = useState(false)
+  const gateway = createGateway({
+    apiKey: import.meta.env.VITE_AI_GATEWAY_API_KEY
+  })
+  const { messages, sendMessage, status, regenerate } = useChat({
+    transport: new DirectChatTransport({
+      agent: new ToolLoopAgent({
+        model: gateway('openai/gpt-5-nano'),
+        instructions:
+          'You are a helpful assistant that can use tools to perform the secret calculations.',
+        tools: {
+          performSecretCalculation: tool({
+            description: 'Performs the secret calculation on two numbers.',
+            inputSchema: z.object({
+              a: z.number(),
+              b: z.number()
+            }),
+            execute: async ({ a, b }) => {
+              console.log(`Performing secret calculation on ${a} and ${b}`)
+              return { result: (a + b) % 10 }
+            }
+          })
+        }
+      })
+    })
+  })
+  const handleSubmit = (message: PromptInputMessage) => {
+    const hasText = Boolean(message.text)
+    const hasAttachments = Boolean(message.files?.length)
+    if (!(hasText || hasAttachments)) {
+      return
+    }
+    sendMessage(
+      {
+        text: message.text || 'Sent with attachments',
+        files: message.files
+      },
+      {
+        body: {
+          model: model,
+          webSearch: webSearch
+        }
+      }
+    )
+    setInput('')
+  }
+  return (
+    <ThemeProvider>
+      <div className="max-w-4xl mx-auto p-6 relative size-full h-screen">
+        <div className="flex flex-col h-full">
+          <Conversation className="h-full scrollable">
+            <ConversationContent>
+              {messages.map((message) => (
+                <div key={message.id}>
+                  {message.role === 'assistant' &&
+                    message.parts.filter((part) => part.type === 'source-url').length > 0 && (
+                      <Sources>
+                        <SourcesTrigger
+                          count={message.parts.filter((part) => part.type === 'source-url').length}
+                        />
+                        {message.parts
+                          .filter((part) => part.type === 'source-url')
+                          .map((part, i) => (
+                            <SourcesContent key={`${message.id}-${i}`}>
+                              <Source key={`${message.id}-${i}`} href={part.url} title={part.url} />
+                            </SourcesContent>
+                          ))}
+                      </Sources>
+                    )}
+                  {message.parts.map((part, i) => {
+                    switch (part.type) {
+                      case 'text':
+                        return (
+                          <Message key={`${message.id}-${i}`} from={message.role}>
+                            <MessageContent>
+                              <MessageResponse>{part.text}</MessageResponse>
+                            </MessageContent>
+                            {message.role === 'assistant' && i === messages.length - 1 && (
+                              <MessageActions>
+                                <MessageAction onClick={() => regenerate()} label="Retry">
+                                  <RefreshCcwIcon className="size-3" />
+                                </MessageAction>
+                                <MessageAction
+                                  onClick={() => navigator.clipboard.writeText(part.text)}
+                                  label="Copy"
+                                >
+                                  <CopyIcon className="size-3" />
+                                </MessageAction>
+                              </MessageActions>
+                            )}
+                          </Message>
+                        )
+                      case 'reasoning':
+                        return (
+                          <Reasoning
+                            key={`${message.id}-${i}`}
+                            className="w-full"
+                            isStreaming={
+                              status === 'streaming' &&
+                              i === message.parts.length - 1 &&
+                              message.id === messages.at(-1)?.id
+                            }
+                          >
+                            <ReasoningTrigger />
+                            <ReasoningContent>{part.text}</ReasoningContent>
+                          </Reasoning>
+                        )
+                      default:
+                        if (part.type.startsWith('tool-')) {
+                          const toolCall = part as ToolUIPart
+                          return (
+                            <Tool>
+                              <ToolHeader state={toolCall.state} type={toolCall.type} />
+                              <ToolContent>
+                                <ToolInput input={toolCall.input} />
+                                {toolCall.state === 'output-available' && (
+                                  <ToolOutput
+                                    errorText={toolCall.errorText}
+                                    output={
+                                      <CodeBlock
+                                        code={JSON.stringify(toolCall.output)}
+                                        language="json"
+                                      />
+                                    }
+                                  />
+                                )}
+                              </ToolContent>
+                            </Tool>
+                          )
+                        }
+                        return null
+                    }
+                  })}
+                </div>
+              ))}
+              {status === 'submitted' && <Loader />}
+            </ConversationContent>
+            <ConversationScrollButton />
+          </Conversation>
+          <PromptInput onSubmit={handleSubmit} className="mt-4" globalDrop multiple>
+            <PromptInputHeader>
+              <PromptInputAttachmentsDisplay />
+            </PromptInputHeader>
+            <PromptInputBody>
+              <PromptInputTextarea onChange={(e) => setInput(e.target.value)} value={input} />
+            </PromptInputBody>
+            <PromptInputFooter>
+              <PromptInputTools>
+                <PromptInputActionMenu>
+                  <PromptInputActionMenuTrigger />
+                  <PromptInputActionMenuContent>
+                    <PromptInputActionAddAttachments />
+                  </PromptInputActionMenuContent>
+                </PromptInputActionMenu>
+                <PromptInputButton
+                  variant={webSearch ? 'default' : 'ghost'}
+                  onClick={() => setWebSearch(!webSearch)}
+                >
+                  <GlobeIcon size={16} />
+                  <span>Search</span>
+                </PromptInputButton>
+                <PromptInputSelect
+                  onValueChange={(value) => {
+                    setModel(value)
+                  }}
+                  value={model}
+                >
+                  <PromptInputSelectTrigger>
+                    <PromptInputSelectValue />
+                  </PromptInputSelectTrigger>
+                  <PromptInputSelectContent>
+                    {models.map((model) => (
+                      <PromptInputSelectItem key={model.value} value={model.value}>
+                        {model.name}
+                      </PromptInputSelectItem>
+                    ))}
+                  </PromptInputSelectContent>
+                </PromptInputSelect>
+              </PromptInputTools>
+              <PromptInputSubmit disabled={!input && !status} status={status} />
+            </PromptInputFooter>
+          </PromptInput>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
 export default App
